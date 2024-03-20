5a8cd8709bc92a26ea1a9017ecfe29d7c6b87e28
Natali Zikrach <mne310872@yandex.ru>
2024-03-20 22:27:53 +0300

    style: style PageToolBar

--- src/components/PageToolbar/PageToolbar.tsx
+++ src/components/PageToolbar/PageToolbar.tsx
@@ -1,8 +1,20 @@
-import Image from 'next/image';
+'use client';
+
 import './PageToolbar.css';
-import StarSvg from '../../../public/assets/star.svg';
+import { useState } from 'react';
 
 export default function PageToolbar() {
+  const [value, setValue] = useState('');
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log('value', value);
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+
   return (
     <div className="page-toolbar">
       <div className="pagetitle-wrap">
@@ -27,13 +39,80 @@ export default function PageToolbar() {
             </svg>
           </div>
         </div>
-        <div className="pagetitle-container">
+        <div className="btn-container">
           <button className="btn-main" type="button">
             Создать
           </button>
           <span className="btn-extra"></span>
         </div>
-        <div className="search-container">input</div>
+        <div className="search-container">
+          <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
+            <label htmlFor="search" className="search-label">
+              <input
+                id="search"
+                type="text"
+                className="inp"
+                placeholder="Фильтр + поиск"
+                autoFocus
+                autoComplete="off"
+                value={value}
+                onChange={(e) => handleChange(e)}
+              />
+              <button type="submit" title="search-btn" className="search-btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-[20px]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  />
+                </svg>
+              </button>
+            </label>
+          </form>
+        </div>
+        <div className="btn-container gap-3">
+          <button type="button" title="calendar-btn" className="calendar-btn">
+            Календари
+          </button>
+          <button type="button" title="setting-btn" className="setting-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="setting-icon"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <hr className="mx-4 h-px bg-white" />
+      <div className="calendar-interface-toolbar">
+        <button type="button" title="counter-panel" className="counter-panel">
+          <div className="counter-panel-value">0</div>
+          <div>Приглашения</div>
+        </button>
+        <button type="button" title="synсh-btn" className="synсh-btn">
+          Синхронизировать календарь
+        </button>
       </div>
     </div>
   );
